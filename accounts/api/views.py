b75cd959f8527d0b1cb018297472510e1e78d431
CL <ghovikusa@gmail.com>
2021-11-26 01:15:15 -0500

    1. Registered accounts url to views.AccountViewSet in urls.py. 2. added accounts in INSTALLED_APPS in settings.py. 3. Init AccountViewSet class in views.py

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,10 +4,23 @@ from rest_framework import permissions
 from rest_framework import viewsets
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    ModelViewSet: provides a form that allows user to POST new user/password
+    ReadOnlyModelViewSet: read user views only
+    """
+
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class AccountViewSet(viewsets.ViewSet):
+    """
+    API endpoint that allows user registration, login, log out,
+    and view login status.
+    """
+    pass
