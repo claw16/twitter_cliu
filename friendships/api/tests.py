9fc363fb17b8385e1c8bab8a87b331989ac58629
CL <ghovikusa@gmail.com>
2022-04-23 11:57:40 +0800

    1. fixed a bug, added a friendships unit test case. 2. added comments model and tests

--- friendships/api/tests.py
+++ friendships/api/tests.py
@@ -1,5 +1,6 @@
 from friendships.models import Friendship
 from testing.testcases import TestCase
+from rest_framework.test import APIClient
 
 
 FOLLOW_URL = '/api/friendships/{}/follow/'
@@ -10,7 +11,21 @@ FOLLOWINGS_URL = '/api/friendships/{}/followings/'
 
 class FriendshipApiTests(TestCase):
     def setUp(self) -> None:
-        self.make_up_friendships()
+        self.anonymous_client = APIClient()
+        self.ann = self.create_user('ann')
+        self.bob = self.create_user('bob')
+        self.ann_client = APIClient()
+        self.bob_client = APIClient()
+        self.ann_client.force_authenticate(self.ann)
+        self.bob_client.force_authenticate(self.bob)
+
+        # create followings and followers for bob
+        for i in range(2):
+            follower = self.create_user(f'bob_follower_{i}')
+            Friendship.objects.create(from_user=follower, to_user=self.bob)
+        for i in range(3):
+            following = self.create_user(f'bob_following_{i}')
+            Friendship.objects.create(from_user=self.bob, to_user=following)
 
     def test_follow(self):
         url = FOLLOW_URL.format(self.ann.id)
@@ -24,6 +39,9 @@ class FriendshipApiTests(TestCase):
         # cannot follow self
         response = self.ann_client.post(url)
         self.assertEqual(response.status_code, 400)
+        # follow a non-existing user
+        response = self.ann_client.post(FOLLOW_URL.format(111))
+        self.assertEqual(response.status_code, 404)
         # a good follow
         response = self.bob_client.post(url)
         self.assertEqual(response.status_code, 201)
