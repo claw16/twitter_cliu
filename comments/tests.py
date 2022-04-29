1a10f914e8c8175a15a48d7f7d473083adb54bb2
CL <ghovikusa@gmail.com>
2022-04-29 16:08:57 +0800

    added likes app

--- comments/tests.py
+++ comments/tests.py
@@ -2,8 +2,20 @@ from testing.testcases import TestCase
 
 
 class CommentModelTests(TestCase):
+    def setUp(self):
+        self.user = self.create_user('ann')
+        self.tweet = self.create_tweet(self.user)
+        self.comment = self.create_comment(self.user, self.tweet)
+
     def test_comment(self):
-        user = self.create_user('ann')
-        tweet = self.create_tweet(user)
-        comment = self.create_comment(user, tweet)
-        self.assertNotEqual(comment.__str__(), None)
+        self.assertNotEqual(self.comment.__str__(), None)
+
+    def test_like_set(self):
+        self.create_like(self.user, self.comment)
+        self.assertEqual(len(self.comment.like_set), 1)
+
+        self.create_like(self.user, self.comment)
+        self.assertEqual(len(self.comment.like_set), 1)
+
+        self.create_like(self.create_user('bob'), self.comment)
+        self.assertEqual(len(self.comment.like_set), 2)
