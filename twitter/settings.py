e12a18ad38bfb3e10a8b80e1f21e64423e97409d
CL <ghovikusa@gmail.com>
2022-05-22 20:31:42 +0800

    Added redis as cache.

--- twitter/settings.py
+++ twitter/settings.py
@@ -177,6 +177,14 @@ CACHES = {
     },
 }
 
+# Redis
+# 安装方法: sudo apt-get install redis
+# 然后安装 redis 的 python 客户端： pip install redis
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+REDIS_DB = 0 if TESTING else 1  # which db, 0: testing, 1: production
+REDIS_KEY_EXPIRE_TIME = 7 * 86400  # in seconds -> 7 days
+
 # 把本地的设置，例如debug配置，放入local_settings.py，不push到remote repo
 # 这样在production环境中不会引入这些设置
 try:
